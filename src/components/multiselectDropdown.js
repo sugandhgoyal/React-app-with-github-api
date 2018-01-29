df23ce105ddba60ad3a388f32e4b4365a7c112d3
Sugandh Goyal <goyalsugandh2@gmail.com>
2018-01-29 17:16:09 +0530

    listing card

--- src/components/multiselectDropdown.js
+++ src/components/multiselectDropdown.js
@@ -15,45 +15,49 @@ class Multidropdown extends React.Component {
             <div>
                 <div className="row row1" >
                     <span id="select1">Choose Interest</span>
-                    <button className="btn btn-default">Art & Culture</button>
-                    <button className="btn btn-default">City Life</button>
-                    <button className="btn btn-default">Events</button>
-                    <button className="btn btn-default">Entertainment</button>
-                    <button className="btn btn-default">Food & Nightlife</button>
-                    <button className="btn btn-default">Fashion</button>
-                    <button className="btn btn-default">Health & Beauty</button>
-                    <button className="btn btn-default">Hobbies & Learning</button>
-                    <button className="btn btn-default">Music</button>
-                    <button className="btn btn-default">Nature & Heritage</button>
-                    <button className="btn btn-default">People & Communities</button>
-                    <button className="btn btn-default">Shopping</button>
-                    <button className="btn btn-default">Student Life</button>
-                    <button className="btn btn-default">Sports</button>
-                    <button className="btn btn-default">Travel</button>
                 </div>
                 <div className="row">
-                    <span>Choose City</span>
-                    <button className="btn btn-default">Andaman</button>
-                    <button className="btn btn-default">Amritsar</button>
-                    <button className="btn btn-default">Agra</button>
-                    <button className="btn btn-default">Coorg</button>
-                    <button className="btn btn-default">Delhi</button>
-                    <button className="btn btn-default">Darjeeling</button>
-                    <button className="btn btn-default">Jaisalmer</button>
-                    <button className="btn btn-default">Kasol</button>
-                    <button className="btn btn-default">Leh Ladakh</button>
-                    <button className="btn btn-default">Mussoorie</button>
-                    <button className="btn btn-default">Mcleodganj & Dharamshala</button>
-                    <button className="btn btn-default">Nasik</button>
-                    <button className="btn btn-default">Nainital</button>
-                    <button className="btn btn-default">Ooty</button>
-                    <button className="btn btn-default">Pondicherry</button>
-                    <button className="btn btn-default">Rishikesh</button>
-                    <button className="btn btn-default">SriNagar</button>
-                    <button className="btn btn-default">Shimla</button>
-                    <button className="btn btn-default">Shillong</button>
-                    <button className="btn btn-default">Udaipur</button>
-                    <button className="btn btn-default">Varanasi</button>
+                    <button className="interests btn btn-default">Art & Culture</button>
+                    <button className="interests btn btn-default">City Life</button>
+                    <button className="interests btn btn-default">Events</button>
+                    <button className="interests btn btn-default">Entertainment</button>
+                    <button className="interests btn btn-default">Food & Nightlife</button>
+                    <button className="interests btn btn-default">Fashion</button>
+                    <button className="interests btn btn-default">Health & Beauty</button>
+                    <button className="interests btn btn-default">Hobbies & Learning</button>
+                    <button className="interests btn btn-default">Music</button>
+                    <button className="interests btn btn-default">Nature & Heritage</button>
+                    <button className="interests btn btn-default">People & Communities</button>
+                    <button className="interests btn btn-default">Shopping</button>
+                    <button className="interests btn btn-default">Student Life</button>
+                    <button className="interests btn btn-default">Sports</button>
+                    <button className="interests btn btn-default">Travel</button>
+                </div>
+                <div className="row">
+                    <span id="select2">Choose City</span>
+                </div>
+                <div className="row">
+                    <button className="city btn btn-default">Andaman</button>
+                    <button className="city btn btn-default">Amritsar</button>
+                    <button className="city btn btn-default">Agra</button>
+                    <button className="city btn btn-default">Coorg</button>
+                    <button className="city btn btn-default">Delhi</button>
+                    <button className="city btn btn-default">Darjeeling</button>
+                    <button className="city btn btn-default">Jaisalmer</button>
+                    <button className="city btn btn-default">Kasol</button>
+                    <button className="city btn btn-default">Leh Ladakh</button>
+                    <button className="city btn btn-default">Mussoorie</button>
+                    <button className="city btn btn-default">Mcleodganj & Dharamshala</button>
+                    <button className="city btn btn-default">Nasik</button>
+                    <button className="city btn btn-default">Nainital</button>
+                    <button className="city btn btn-default">Ooty</button>
+                    <button className="city btn btn-default">Pondicherry</button>
+                    <button className="city btn btn-default">Rishikesh</button>
+                    <button className="city btn btn-default">SriNagar</button>
+                    <button className="city btn btn-default">Shimla</button>
+                    <button className="city btn btn-default">Shillong</button>
+                    <button className="city btn btn-default">Udaipur</button>
+                    <button className="city btn btn-default">Varanasi</button>
                 </div>
             </div >
         )
